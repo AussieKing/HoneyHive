7308f561d34c96e1f8c665d92ba8a08ffbf8c2c9
freddydordoni <freddydordoni@Mac-mini.local>
2024-01-10 17:55:42 +1000

    Complete Day 1: Initialized React app and set up project structure

---
+++ honey-and-hive/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// importing bootstrap below and React above
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World! 🌏  </h1>
+    </div>
+  );
+}
+
+export default App;
