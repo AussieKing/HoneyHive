7032de6b7507b3cef416e0eee80dc2f6b8286977
freddydordoni <freddydordoni@Mac-mini.local>
2024-01-20 18:20:59 +1000

    Implemented full-page dropdown menu with smooth animation and fixed logo font issue

--- honey-and-hive/src/components/Header.js
+++ honey-and-hive/src/components/Header.js
@@ -1,41 +1,64 @@
-import React from 'react';
-import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle menu open/close
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <Navbar bg="transparent" variant="light" fixed="top">
-      <Container>
-        <Nav className="me-auto">
-          <Dropdown>
-            <Dropdown.Toggle variant="transparent" id="dropdown-basic" className="dropdown-toggle">
-              <FontAwesomeIcon icon={faBars} />
-            </Dropdown.Toggle>
+    <>
+      {/* Navbar structure */}
+      <Navbar bg="transparent" variant="light" fixed="top">
+        <Container>
+          {/* Menu icon (burger menu)*/}
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={toggleMenu}
+            className="header-icon"
+          />
 
-            <Dropdown.Menu className="dropdown-menu">
-              <Dropdown.Item as={Link} to="/about">About</Dropdown.Item>
-              <Dropdown.Item as={Link} to="/services">Services</Dropdown.Item>
-              <Dropdown.Item as={Link} to="/projects">Projects</Dropdown.Item>
-              <Dropdown.Item as={Link} to="/contact">Contact</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Nav>
+          <Navbar.Brand href="/" className="logo-font">
+            {" "}
+            {/* Because the logo is styled, we need to make sure the class here matches App.css */}
+            HONEY & HIVE
+          </Navbar.Brand>
 
-        <Navbar.Brand href="/" className="mx-auto">
-          HONEY & HIVE
-        </Navbar.Brand>
+          {/* Contact link */}
+          <Nav className="ms-auto">
+            <Link to="/contact" className="nav-link">
+              CONTACT
+            </Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-        <Nav className="ms-auto">
-          <Link to="/contact" className="nav-link">
-            CONTACT
+      {/* Menu overlay */}
+      <div className={`menu-overlay ${isMenuOpen ? "show" : ""}`}>
+        <div className="menu-content">
+          {/* Menu links */}
+          <Link to="/about" onClick={toggleMenu}>
+            About
           </Link>
-        </Nav>
-      </Container>
-    </Navbar>
+          <Link to="/services" onClick={toggleMenu}>
+            Services
+          </Link>
+          <Link to="/projects" onClick={toggleMenu}>
+            Projects
+          </Link>
+          <Link to="/contact" onClick={toggleMenu}>
+            Contact
+          </Link>
+        </div>
+      </div>
+    </>
   );
 };
 
 export default Header;
-
