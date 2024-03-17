6a3c65ccd3da6199aefe8ccb275bfd341d5e8308
freddydordoni <freddydordoni@protonmail.com>
2024-03-17 21:44:26 +1000

    Remove unused Strapi directory

--- honey-and-hive-strapi/test-db-connection.js
+++
@@ -1,6 +0,0 @@
-const parse = require('pg-connection-string').parse;
-// Replace the string below with your actual DATABASE_URL value
-const connectionString = 'postgres://freddydordoni:%40shley7B3@localhost:5432/strapi';
-const config = parse(connectionString);
-
-console.log(config);
